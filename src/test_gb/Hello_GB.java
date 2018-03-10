4b0745a097c0ee4380b670d3856fbbed590e8967
Gerd <Gerd@Gerd-PC.mshome.net>
2018-03-10 14:44:26 +0100

    Loop change ...

--- src/test_gb/Hello_GB.java
+++ src/test_gb/Hello_GB.java
@@ -4,7 +4,9 @@ public class Hello_GB {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("Hello World!!!");
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i + " Hello World!!!");			
+		}		
 	}
-
+	
 }
