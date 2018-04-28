b8e56dbeb6e2ae4a262f2dd0c8c7cc139a3ff2ab
Gerd <Gerd@Gerd-PC.fritz.box>
2018-04-28 11:13:09 +0200

    ... switch to Maven project ...

--- src/test_gb/Hello_GB.java
+++ src/test_gb/Hello_GB.java
@@ -1,12 +1,21 @@
 package test_gb;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Hello_GB {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		
+		String[] ar = new String[5];
+		ar[0] = "Hello World!!!";
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("Hello World!!!");
+		HashMap<String, String> hm = new HashMap<String, String>();
+		hm.put("Key", "Hello World!!!");
 		for (int i = 0; i < 100; i++) {
 			System.out.println(i + " Hello World!!!");			
-		}		
+		}
 	}
 	
 }
